bf7bf77fc03c872c71f969285b657f1c380c61ad
Michael.Mittermaier <mittemic>
2020-01-27 14:42:38 +0000

    changed onCreate method

--- app/src/main/java/ie/ul/firebaseauthdemo/MainActivity.java
+++ app/src/main/java/ie/ul/firebaseauthdemo/MainActivity.java
@@ -28,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Michael changed the onCreate function
+        if (true){
+            System.out.println(":)");
+        }
     }
 
     @Override
